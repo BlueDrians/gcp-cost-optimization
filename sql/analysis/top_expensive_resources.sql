1b6af57d8105bbe02e5efcd99abdf1ddbd0c70f4
BlueDrians <yadrianmaulana@gmail.com>
2026-06-10 16:34:12 +0700

    Update top_expensive_resources.sql

--- sql/analysis/top_expensive_resources.sql
+++ sql/analysis/top_expensive_resources.sql
@@ -4,7 +4,7 @@ SELECT
   service_description,
   resource_name,
   ROUND(SUM(net_cost), 2) AS total_net_cost
-FROM `YOUR_PROJECT_ID.finops.billing_export_sample`
+FROM `PROJECT_ID.finops.billing_export_sample`
 WHERE usage_start_date BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY) AND CURRENT_DATE()
 GROUP BY project_id, service_description, resource_name
 ORDER BY total_net_cost DESC
