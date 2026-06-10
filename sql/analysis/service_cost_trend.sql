d3f98b32d28aa84777cb9c9ec89a6589418e2acc
BlueDrians <yadrianmaulana@gmail.com>
2026-06-10 16:33:59 +0700

    Update service_cost_trend.sql

--- sql/analysis/service_cost_trend.sql
+++ sql/analysis/service_cost_trend.sql
@@ -3,6 +3,6 @@ SELECT
   usage_start_date,
   service_description,
   ROUND(SUM(net_cost), 2) AS daily_net_cost
-FROM `YOUR_PROJECT_ID.finops.billing_export_sample`
+FROM `PROJECT_ID.finops.billing_export_sample`
 GROUP BY usage_start_date, service_description
 ORDER BY usage_start_date, service_description;
