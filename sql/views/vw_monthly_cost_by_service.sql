f3e33b5e1f3b3fc67ce7430bb37bc07dccd020ef
BlueDrians <yadrianmaulana@gmail.com>
2026-06-10 16:35:15 +0700

    Update vw_monthly_cost_by_service.sql

--- sql/views/vw_monthly_cost_by_service.sql
+++ sql/views/vw_monthly_cost_by_service.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW `YOUR_PROJECT_ID.finops.vw_monthly_cost_by_service` AS
+CREATE OR REPLACE VIEW `PROJECT_ID.finops.vw_monthly_cost_by_service` AS
 SELECT
   FORMAT_DATE('%Y-%m', usage_start_date) AS billing_month,
   service_description,
@@ -8,5 +8,5 @@ SELECT
   ROUND(SUM(cost), 2) AS gross_cost,
   ROUND(SUM(credits), 2) AS credits,
   ROUND(SUM(net_cost), 2) AS net_cost
-FROM `YOUR_PROJECT_ID.finops.billing_export_sample`
+FROM `PROJECT_ID.finops.billing_export_sample`
 GROUP BY billing_month, service_description, project_id, environment, team;
